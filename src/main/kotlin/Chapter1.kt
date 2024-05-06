7d3c8cd59497e4ef13f18d5446d02fd4b7ebc0d2
Alva Tran <ceilingprogressproductions2001@gmail.com>
2024-05-06 17:40:53 +0700

    Initial commit

---
+++ src/main/kotlin/Chapter1.kt
@@ -0,0 +1,5 @@
+//Chapter 1: Kotlin Development Environment
+
+//Allows to program on a number of different platforms. The two most-prominent platforms are the Java Virtual Machine (JVM) and Android.
+//Java programs compile into bytecode, which runs on the Java Virtual Machine (JVM). The JVM acts as a layer above the machine, enabling Java code to be shared across different computer systems.
+//100% interoperable with the Java
